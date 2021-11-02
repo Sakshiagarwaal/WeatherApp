0e95b4d855ae42ef101e06f913c5873e370e2cdb
Sakshi Garg <sakshigarg0180@gmail.com>
2021-11-02 14:01:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        className="searchBox"
+        type={Text}
+        placeHolder="Type City Name"
+      ></input>
+      <div>
+        <div className="location">New York,US </div>
+        <div className="date">Tuesday 2 November 2021</div>
+      </div>
+      <div className="temperature">10°c</div>
+      <div className="temp">
+        <div className="min temp_sibling">Min 4°c </div>
+        <div className="max temp_sibling">Max 13°c</div>
+      </div>
+      <div className="weather">Clouds</div>
+      <div>
+        <button>Show More </button>
+        <div>
+          <div> Wind Speed 0km/miles</div>
+        </div>
+      </div>
     </div>
   );
 }
