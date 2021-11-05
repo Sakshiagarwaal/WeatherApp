bdc513ccf4db724b7ef8de528c8671d9b8837298
Sakshi Garg <sakshigarg0180@gmail.com>
2021-11-05 21:58:49 +0530

    api connected

--- src/App.js
+++ src/App.js
@@ -1,32 +1,90 @@
-import React from "react";
+import React , {useState , useEffect} from "react";
 import "./App.css";
+import moment from 'moment'
+import {format} from 'date-fns';
+const api = {
+  key: "fff592fae2e8ed5526bbf703869c8305",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
 
 function App() {
+    const [result , setResult]= useState();
+    const [city , setCity] =useState('delhi');
+    const [show , setShow] =useState(false);
+    var date = new Date();
+    var formattedDate = format( date, "MMMM dd , yyyy, H:mma");
+    console.log("second console",formattedDate);
+  //  function getParsedDate(strDate) {
+  //     var strSplitDate = String(strDate).split(' ');
+  //     var date = new Date(strSplitDate[0]);
+  //     // alert(date);
+  //     var dd = date.getDate();
+  //     var mm = date.getMonth() + 1; //January is 0!
+  
+  //     var yyyy = date.getFullYear();
+  //     if (dd < 10) {
+  //         dd = '0' + dd;
+  //     }
+  //     if (mm < 10) {
+  //         mm = '0' + mm;
+  //     }
+  //     date =  dd + "-" + mm + "-" + yyyy;
+  //     return date.toString();
+  // }
+  // console.log("date" , getParsedDate(new Date()))
+    useEffect(() => {
+      fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`)
+      .then(response => response.json())
+      .then(data => (setResult(data)))
+         } , [])
+    const togelHandler=()=>{
+      setShow(!show);
+    }
+    const clickHandler = (e)=>{
+      setCity(e.target.value);
+    }
+   const keyHandler=(e)=>{
+        if(e.key ==="Enter"){
+          fetch(`${api.base}weather?q=${city}&units=metric&appid=${api.key}`)
+          .then(response => response.json())
+          .then(data => (setResult(data)));
+  }
+          
+        }
+    console.log("first console" ,new Date());
   return (
-    <div className="App">
-      <input
+    <div className={result?.main.temp <20?'App':'appSummer'}>
+      <input onChange={(e) =>{clickHandler(e)}}
+       onKeyPress={(e)=>{keyHandler(e)}}
+
+
         className="searchBox"
         type={Text}
         placeHolder="Type City Name"
       ></input>
       <div>
-        <div className="location">New York,US </div>
-        <div className="date">Tuesday 2 November 2021</div>
+        <div className="location">{result?.name},{result?.sys.country} </div>
+        <div className="date">{formattedDate}</div>
       </div>
-      <div className="temperature">10°c</div>
+      <div className="temperature">{Math.round(result?.main.temp)}°C </div>
       <div className="temp">
-        <div className="min temp_sibling">Min 4°c </div>
-        <div className="max temp_sibling">Max 13°c</div>
+        <div className="min temp_sibling">Min {Math.round(result?.main.temp_min)}°C </div>
+        <div className="max temp_sibling">Max {Math.round(result?.main.temp_max)}°C </div>
       </div>
-      <div className="weather">Clouds</div>
+      <div className="weather">{result?.weather[0].main}</div>
       <div>
-        <button>Show More </button>
-        <div>
-          <div> Wind Speed 0km/miles</div>
+        <button className="button"onClick={togelHandler}> {show?'Show Less':'Show More'} </button>
+        <div className="application">
+         {show&&<><div>Wind Speed: {result?.wind.speed}km/miles</div>
+          <div>Humidity: {result?.main.humidity}%</div>
+          <div>Pressure: {result?.main.pressure}Pa</div>           
+          <div>sunrise: {moment.utc(result?.sys.sunrise).format().split("T")[1]}</div>
+          <div>Sunset: {moment.utc(result?.sys.sunset).format().split("T")[1]}</div></>}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
